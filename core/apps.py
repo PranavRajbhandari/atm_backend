c1ed0e145029bba59991dee09676f94fb92f1640
pranav <pranavrajbhandari123@gmail.com>
2025-08-20 17:51:55 +0545

    serializers.py and app.py

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,24 @@
+# atm_backend/core/apps.py
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+
+def ready(self):
+    import core.signals # noqa
+
+# atm_backend/core/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Account
+
+default_app_config = 'core.apps.CoreConfig'
+
+@receiver(post_save, sender=User)
+def create_account(sender, instance, created, **kwargs):
+    if created:
+        Account.objects.create(user=instance, balance=0)
---
+++ core/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Account, Transaction
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+
+    class Meta:
+        model = Account
+        fields = ['id', 'user', 'balance']
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = ['id', 'txn_type', 'amount', 'balance_after', 'note', 'created_at']
+
+
+class MoneyActionSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(max_digits=12, decimal_places=2)
+
+
+    class TransferSerializer(serializers.Serializer):
+        to_username = serializers.CharField()
+        amount = serializers.DecimalField(max_digits=12, decimal_places=2)
